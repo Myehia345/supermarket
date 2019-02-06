9d4d07363b1ebe6a2acac3b7a5f27e37a2bbb071
Mahmoud Yehia <eng.mdodo94@gmail.com>
2019-02-06 12:58:12 +0000

    full user info, design configration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190204122316) do
+ActiveRecord::Schema.define(version: 20190206001344) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20190204122316) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "phone"
+    t.string   "address"
+    t.string   "username"
+    t.string   "type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
